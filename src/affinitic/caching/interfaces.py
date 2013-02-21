d6f6d45edc5f32294cbfe78bca87e125f115656a
Jean-François Roche <jfroche@affinitic.be>
2013-02-21 21:37:35 +0100

    merge

---
+++ src/affinitic/caching/interfaces.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+"""
+interfaces.py
+
+Created by martinpeeters on 2011-08-24.
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+"""
+
+from zope.component import Interface
+
+
+class IInvalidateKey(Interface):
+    """
+    """
